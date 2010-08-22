2a39bc4cb0dda0b4364f3768d29064ba8bdb8030
MacBook Pro <doug.gardiner@gmail.com>
2010-08-22 07:26:14 -0500

    Moved file operations until after everything is received on the net. Otherwise empty files result and they overwrite the old.  NOT GOOD.

--- src/Debug.java
+++ src/Debug.java
@@ -7,57 +7,55 @@ public class Debug {
     // class variables
 
     JTextArea output = null;
-	JFrame frame;
+    JFrame frame;
     // constructors
 
     public Debug() {
 
-	frame = new JFrame("Debug output");
-	output = new JTextArea(10,30);
-	JPanel outputPanel = new JPanel();
-	JScrollPane messageScroller = new JScrollPane(output);
-	String destination = "net.sourceforge.napkinlaf.NapkinLookAndFeel";
-	try {
-		UIManager.setLookAndFeel(destination);
-    } catch (ClassNotFoundException es) {
-		es.printStackTrace();
-    } catch (InstantiationException et) {
-		System.out.println("cannot initialize what I cannot find");
-    } catch (IllegalAccessException er) {
-		System.out.println("Now sure what to say anymore");
-    } catch (UnsupportedLookAndFeelException ev) {
-		System.out.println("cannot do " + destination);
-		ev.printStackTrace();
-    }
-	// build the screen
-
-	outputPanel.add(messageScroller);
-	outputPanel.setBorder(BorderFactory.createTitledBorder(
-	     BorderFactory.createEtchedBorder(), "Debug Messages"));
-	messageScroller.setHorizontalScrollBarPolicy(
-	     ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-	messageScroller.setVerticalScrollBarPolicy(
-	     ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-	output.setLineWrap(true);
-	output.setWrapStyleWord(true);
-	BorderLayout layout = new BorderLayout();
-	JPanel background = new JPanel(layout);
-	JPanel contents = new JPanel();
-	background.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-	contents.add(outputPanel);
-	background.add(contents);
-	frame.getContentPane().add(background);
-	frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	SwingUtilities.updateComponentTreeUI(frame);
-	// ugly hack, getBounds result in issues with both windows?
-	frame.setLocation(0,130);
-	frame.pack();
-	frame.setVisible(true);
+        frame = new JFrame("Debug output");
+        output = new JTextArea(10,30);
+        JPanel outputPanel = new JPanel();
+        JScrollPane messageScroller = new JScrollPane(output);
+        String destination = "net.sourceforge.napkinlaf.NapkinLookAndFeel";
+        try {
+            UIManager.setLookAndFeel(destination);
+        } catch (ClassNotFoundException es) {
+            es.printStackTrace();
+        } catch (InstantiationException et) {
+            System.out.println("cannot initialize what I cannot find");
+        } catch (IllegalAccessException er) {
+            System.out.println("Now sure what to say anymore");
+        } catch (UnsupportedLookAndFeelException ev) {
+            System.out.println("cannot do " + destination);
+            ev.printStackTrace();
+        }
+        // build the screen
+        
+        outputPanel.add(messageScroller);
+        outputPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Debug Messages"));
+        messageScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        messageScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        output.setLineWrap(true);
+        output.setWrapStyleWord(true);
+        BorderLayout layout = new BorderLayout();
+        JPanel background = new JPanel(layout);
+        JPanel contents = new JPanel();
+        background.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        contents.add(outputPanel);
+        background.add(contents);
+        frame.getContentPane().add(background);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        SwingUtilities.updateComponentTreeUI(frame);
+    
+        // ugly hack, getBounds result in issues with both windows?
+        frame.setLocation(0,130);
+        frame.pack();
+        frame.setVisible(true);
     }
     // methods
 
     public void update(String newMessage) {
 
-	output.append(newMessage + "\n");
+        output.append(newMessage + "\n");
     }
 }

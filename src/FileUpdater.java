5a649415ade6aa3d54c0cba3375221615e206f23
MacBook Pro <doug.gardiner@gmail.com>
2010-10-17 12:14:31 -0500

    change update direction

--- src/FileUpdater.java
+++ src/FileUpdater.java
@@ -135,7 +135,7 @@ public class FileUpdater implements Runnable {
             if (inTheACL) {
                 debugger.update (" ---- " + remoteAddress.getHostName() +  " is in the List");
 // TS spot
-			if (remoteDate == 0 || diffInTime > 0) {
+			if (remoteDate == 0 || diffInTime < 0) {
 //
     		// local file is newer or doesnt exist so transmit this one
 
@@ -169,7 +169,7 @@ public class FileUpdater implements Runnable {
 
 				}
 // TS spot
-                if (beacon.getFileDate() == 0 || diffInTime < 0) {
+                if (beacon.getFileDate() == 0 || diffInTime > 0) {
 //
                     // local file is older
             

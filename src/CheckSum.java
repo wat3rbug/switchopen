a112714e14ac310feb93b45734c4bfcee995089a
MacBook Pro <doug.gardiner@gmail.com>
2010-10-22 13:16:41 -0500

    remove seperate method

--- src/CheckSum.java
+++ src/CheckSum.java
@@ -4,37 +4,30 @@ import java.security.MessageDigest;
 public class CheckSum {
  
 	// class variables
-
-	byte [] mdbytes = null;
 	
-	// constructors
 	
-	public CheckSum(String filename) {
+	// methods
+	
+	public static String CheckSum(String filename) {
 		
-		MessageDigest md = null;
+		StringBuffer buffer = new StringBuffer("");
 		try {	
-			md = MessageDigest.getInstance("SHA1");
+			MessageDigest md = MessageDigest.getInstance("SHA1");
 	    	FileInputStream fis = new FileInputStream(filename);
 			byte[] dataBytes = new byte[1024];
+			
 			int nread = 0;
 			while ((nread = fis.read(dataBytes)) != -1) {
 				md.update(dataBytes, 0, nread);
 			}
-			mdbytes = md.digest();
-		} catch (Exception ioe) {
+			byte[] mdbytes = md.digest();
+			for (int i = 0; i < mdbytes.length; i++) {
+				buffer.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+			}
 			
-		}
-	}
-	// methods
-	
-	public String update() {
-		
-		// convert byte to hex and return
-		
-		StringBuffer buffer = new StringBuffer("");
-		for (int i = 0; i < mdbytes.length; i++) {
-			buffer.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+		} catch (Exception ioe) {
+			// do what?
 		}
 		return buffer.toString();
-	} 
+	}
 }

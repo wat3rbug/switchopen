b84a4455ccfdb4ba65d2af7dd7a2704aee972a48
MacBook Pro <doug.gardiner@gmail.com>
2010-10-23 07:57:23 -0500

    fix debug statement in regular code

--- src/FileUpdater.java
+++ src/FileUpdater.java
@@ -209,7 +209,7 @@ public class FileUpdater implements Runnable {
                     	}
                 	}
 				} else {
-					debugger.update(" -- in ACL but CRC is the same");
+					if (debug) debugger.update(" -- in ACL but CRC is the same");
 				}
             } else { // not in ACL
                 if (debug) {

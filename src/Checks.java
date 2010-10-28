509a85a6449e1e6ee0589b37b6320ffb164b0ab3
MacBook Pro <doug.gardiner@gmail.com>
2010-10-28 10:34:17 -0500

    changed the name to more appropriate use

---
+++ src/Checks.java
@@ -0,0 +1,50 @@
+// Created by: Douglas Gardiner
+// Creation Date: Fri Oct 22 13:21:14 CDT 2010
+// Update Date: Sat Oct 23 08:07:26 CDT 2010
+//
+//@author Douglas Gardiner
+
+/** Used for getting a checksum hash of a file
+ * @author Douglas Gardiner
+ */
+
+import java.io.FileInputStream;
+import java.security.MessageDigest;
+import java.lang.String;
+
+ 
+public class Checks {
+ 
+	// class variables
+	
+	// methods
+	
+/**
+ * returns the String representation of the SHA1 hash of the filename as a String
+ * @return buffer - String representation of the SHA1 hash in hexidecial form.
+ * @param filename String representation of the filename to be used.
+ */
+	public static String update(String filename) {
+		
+		StringBuffer buffer = new StringBuffer("");
+		try {	
+			MessageDigest md = MessageDigest.getInstance("SHA1");
+	    	FileInputStream fis = new FileInputStream(filename);
+			byte[] dataBytes = new byte[1024];
+			
+			int nread = 0;
+			while ((nread = fis.read(dataBytes)) != -1) {
+				md.update(dataBytes, 0, nread);
+			}
+			byte[] mdbytes = md.digest();
+			for (int i = 0; i < mdbytes.length; i++) {
+				buffer.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			
+		} catch (Exception ioe) {
+			// do what? file failed send 0 out
+			buffer= new StringBuffer("0");
+		} 
+		return buffer.toString();
+	}
+}

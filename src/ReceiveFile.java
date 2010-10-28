da184e53028b91ace9b1990b47817a169e853bea
MacBook Pro <doug.gardiner@gmail.com>
2010-10-28 11:48:28 -0500

    moved some checks around and verified again before receive.

--- src/ReceiveFile.java
+++ src/ReceiveFile.java
@@ -35,7 +35,7 @@ public class ReceiveFile {
 	/**
  	 * Creates a ReceiveFile object with a reference to the frame for the main GUI and debug window for updates.
 	 * @param frame reference to the main GUI for updates.
-	 * @param passedfram reference for the debug window
+	 * @param passedframe reference for the debug window
  	 */
 
     public ReceiveFile(JFrame frame, Debug passedframe) {
@@ -68,6 +68,7 @@ public class ReceiveFile {
         ServerSocket socket = null;
         success = true;
         try {
+			Checks securityStuff = new Checks();
             if (debug) debugger.update(" --- opening TCP socket --- ");
             socket = new ServerSocket(port);
 			socket.setReuseAddress(true);
@@ -78,76 +79,82 @@ public class ReceiveFile {
             }
             socket.setSoTimeout(SEC_LENGTH * 15); // 15 sec retry for listen so the rest of the app isn't hung
             Socket connection = socket.accept();
-            if (debug) debugger.update(" -- heard distant end");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine = null;
-            if (debug) debugger.update(" --- receiving file --- ");
+			InetAddress doubleCheck = connection.getInetAddress();
+			if (securityStuff.exists() && securityStuff.inACL(doubleCheck)) {
+            	if (debug) debugger.update(" -- heard distant end");
+            	BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            	String inputLine = null;
+            	if (debug) debugger.update(" --- receiving file --- ");
 
-			// stream accepted and now reading contents to file
+				// stream accepted and now reading contents to file
 			
-            ArrayList <String> fileContents = new ArrayList<String>();
-            while (!(inputLine = reader.readLine()).equals(EOF)) {
-                fileContents.add(inputLine);
-            }
-            reader.close();
-            socket.close();
+            	ArrayList <String> fileContents = new ArrayList<String>();
+            	while (!(inputLine = reader.readLine()).equals(EOF)) {
+                	fileContents.add(inputLine);
+            	}
+            	reader.close();
+            	socket.close();
    
-			// make sure not to create an empty file
+				// make sure not to create an empty file
 			
-            File newFile = new File(filename);
-            if (newFile.exists() ) {
-                newFile.delete();
-            } 
-            newFile.createNewFile();
-            BufferedWriter writer = new BufferedWriter(new FileWriter(newFile));
-            for (int i = 0; i < fileContents.size() ; i++) {
-                writer.write(fileContents.get(i) + "\r\n");
-                if (debug) debugger.update(" ---- wrote " + fileContents.get(i));
-            }
-            if (debug) debugger.update(" --- closing file and TCP socket --- ");
-            fileContents.clear();
-            writer.flush();
-            writer.close();         
-            if (debug) {
-                debugger.update(Calendar.getInstance().getTime() + "\n --- Leaving receive file process ---");
+            	File newFile = new File(filename);
+            	if (newFile.exists() ) {
+                	newFile.delete();
+            	} 
+            	newFile.createNewFile();
+            	BufferedWriter writer = new BufferedWriter(new FileWriter(newFile));
+            	for (int i = 0; i < fileContents.size() ; i++) {
+                	writer.write(fileContents.get(i) + "\r\n");
+                	if (debug) debugger.update(" ---- wrote " + fileContents.get(i));
+            	}
+            	if (debug) debugger.update(" --- closing file and TCP socket --- ");
+            	fileContents.clear();
+            	writer.flush();
+            	writer.close();         
+            	if (debug) {
+                	debugger.update(Calendar.getInstance().getTime() + "\n --- Leaving receive file process ---");
+				}
+            	JOptionPane.showMessageDialog(frame, "Update successful", "SwitchFinder Update", JOptionPane.INFORMATION_MESSAGE);
+				success = true;
+			} else {
+				success = false;
 			}
-            JOptionPane.showMessageDialog(frame, "Update successful", "SwitchFinder Update", JOptionPane.INFORMATION_MESSAGE);
-        } catch (FileNotFoundException fnfe) {
-            JOptionPane.showMessageDialog(frame, "Something is blocking\nwrite permissions to " + filename, "File in Use", JOptionPane.ERROR_MESSAGE);
-            if (debug) {
-                fnfe.printStackTrace();
-            }
-            success = false;
-        } catch (SocketTimeoutException ste) {
-            if (debug) debugger.update("Timeout occurred");
-            success = false;
-        } catch (BindException be) {
-            if (! socket.isClosed()) socket.isClosed();
-            success = false;
-            if (debug) be.printStackTrace();
-        } catch (SecurityException se) {
-            JOptionPane.showMessageDialog(frame, "No permissions to write read this file", "File Permissions", JOptionPane.ERROR_MESSAGE);
-            if (debug) {
-                debugger.update("--- Receive File failure ---");
-                se.printStackTrace();
-            }
-            success = false;
-        } catch (IOException ioe) {
-            if (debug) {
-                debugger.update("--- Receive File failure ---");
-                ioe.printStackTrace();
-            }
-            success = false;
-        } finally {
-            if (! socket.isClosed()) {
-                try {
-                    socket.close();
-                } catch (IOException ioe) {
-                    // do nothing as this is utterly useless.  If there isn't one go quiety
-                }
-            }
-            // return success;
-        }
-        return success;
+       	} catch (FileNotFoundException fnfe) {
+           	JOptionPane.showMessageDialog(frame, "Something is blocking\nwrite permissions to " + filename, "File in Use", JOptionPane.ERROR_MESSAGE);
+            	if (debug) {
+               		fnfe.printStackTrace();
+            	}
+           	success = false;
+       	} catch (SocketTimeoutException ste) {
+           	if (debug) debugger.update("Timeout occurred");
+           	success = false;
+       	} catch (BindException be) {
+           	if (! socket.isClosed()) socket.isClosed();
+           	success = false;
+           	if (debug) be.printStackTrace();
+       	} catch (SecurityException se) {
+           	JOptionPane.showMessageDialog(frame, "No permissions to write read this file", "File Permissions", JOptionPane.ERROR_MESSAGE);
+           	if (debug) {
+               	debugger.update("--- Receive File failure ---");
+               	se.printStackTrace();
+           	}
+           	success = false;
+       	} catch (IOException ioe) {
+           	if (debug) {
+               	debugger.update("--- Receive File failure ---");
+               	ioe.printStackTrace();
+           	}
+           	success = false;
+       	} finally {
+           	if (! socket.isClosed()) {
+               	try {
+                   	socket.close();
+               	} catch (IOException ioe) {
+                   	// do nothing as this is utterly useless.  If there isn't one go quiety
+               	}
+           	}
+           	// return success;
+       	}
+		return success;
     }
 }

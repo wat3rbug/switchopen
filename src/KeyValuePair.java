03f6aebeb3b0dcf9ba1338f7cc05f0e8ba3beb86
Douglas Gardiner <Doug.Gardiner@gmail.com>
2011-12-01 14:27:45 -0600

    removed some debug noise

--- src/KeyValuePair.java
+++ src/KeyValuePair.java
@@ -94,12 +94,9 @@ public class KeyValuePair {
 	public String getKey(String address) {
 		
 		int index = addresses.indexOf(address);
-		update("address given: " + address);
 		if (index < 0) {
-			update("name: not found");
 			return "not found";
 		} else {
-			update("name: " + names.get(index));
 			return names.get(index);
 		}
 	}
